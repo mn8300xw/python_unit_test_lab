3d51c88ceba837fd120b0baf48bba9b2e3d13dce
Vanessa M. <mn8300xw@go.minnstate.edu>
2026-02-25 14:23:37 -0600

    added additional tests scenarios

--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -32,7 +32,40 @@ class TestDiscount(TestCase):
     def test_null_input(self):
         self.fail('must have an entry to calculate discount')
 
+    def test_list_of_one_price(self):
+        self.fail("This test needs to be implemented")
 
+    # 10
+    def test_list_of_negative_prices(self):
+        self.fail("This test needs to be implemented")
+
+    # 11 [0 ,0, 0, 0]
+    def test_list_of_zeros_prices(self):
+        self.fail("This test needs to be implemented")
+
+    # 12
+    def test_list_of_floats_prices(self):
+        self.fail("This test needs to be implemented")
+
+    # 13
+    def test_list_of_mixed_int_and_float_prices(self):
+        self.fail("This test needs to be implemented")
+
+    # 14
+    def test_all_items_same_price(self):
+        self.fail("This test needs to be implemented")
+
+    # 15 Ex: [10000000000000000000000 ** 100000000000000000000000 , 4, 20]
+    def test_list_of_large_numbers(self):
+        self.fail("This test needs to be implemented")
+
+    # 16 [10 / 0, 4, 20]
+    def test_list_with_zero_divisor(self):
+        self.fail("This test needs to be implemented")
+
+    # 17 ['0', '4', '20']
+    def test_list_with_string_prices(self):
+        self.fail("This test needs to be implemented")
 
 if __name__ == '__main__':
     unittest.main()
